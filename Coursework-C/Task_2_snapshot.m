517c36f4a2249f5950a108438ef57f3da8bc089c
sharwinbobde <bsharwin@gmail.com>
2020-03-14 21:11:45 +0100

    generalized TAsk 2 code for any dataset

--- Coursework-C/Task_2_snapshot.m
+++ Coursework-C/Task_2_snapshot.m
@@ -9,27 +9,38 @@ Y = center_points(X);
 size(Y,2)
 
 G = compute_gram_matrix(Y);
-disp(G)
 
 d=2; % reduced to 2 dimentions
 mapping = compute_mapping(G, Y, d);
 
 X_low_dim = X * mapping;
 
+plot_2d_scatter(X_low_dim, labels);
+
+
+% #######################################################################################################
+
 
 %%
 
-complot = figure();
-iris_setosa_indexes = find(strcmp(labels, 'Iris-setosa'));
-iris_versicolor_indexes = find(strcmp(labels, 'Iris-versicolor'));
-iris_virginica_indexes = find(strcmp(labels, 'Iris-virginica'));
+function plot_2d_scatter(x, labels)
+    classes = unique(labels);
+    num_classes = size(classes, 1);
+    
+    colmap = ["r", "g", "b", "y", "m"];
+    
+    figure();
+    for i = 1:num_classes
+        indexes = find(strcmp(labels, classes(i)));
+    
+        scatter(x(indexes,1), x(indexes,2), colmap(i));
+        hold on
+        c = mean(x(indexes,:));
+        text(c(1), c(2), classes(i))
+        hold on
+    end
+end
 
-scatter(X_low_dim(iris_setosa_indexes,1), X_low_dim(iris_setosa_indexes,2), 'red')
-hold on
-scatter(X_low_dim(iris_versicolor_indexes,1), X_low_dim(iris_setosa_indexes,2), 'blue')
-hold on
-scatter(X_low_dim(iris_virginica_indexes,1), X_low_dim(iris_setosa_indexes,2), 'green')
-grid on
 
 %%
 function [x_c] = center_points(x)
